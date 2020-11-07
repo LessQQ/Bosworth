0a7bc48ed16b1f58ae7f3959894cd1425849b530
Robb Vogel <robert.d.vogel@gmail.com>
2020-11-07 06:24:41 -0600

    Document

---
+++ Bosworth/BosworthDocument.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+using Nottingham;
+using Nottingham.Shire;
+
+namespace Bosworth
+{
+    public class BosworthDocument
+    {
+        private ShireDB shireDB { get; set; }
+        private string FileName { get; set; }
+
+        public BosworthDocument(string fileName = "default.bos")
+        {
+            FileName = fileName;
+            shireDB = new ShireDB(FileName);
+        }
+    }
+}
